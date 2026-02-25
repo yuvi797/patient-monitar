eb6aff18b757fed0d2fdf623fdad092f5055faae
Yuvraj Sinha <yuvraj@gmail.com>
2026-02-25 10:20:21 +0530

    fix frontend path

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve frontend folder
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-// API Route
 app.get("/api/data", (req, res) => {
   res.json({
-    heartRate: (60 + Math.random() * 40).toFixed(0),
-    water: (40 + Math.random() * 60).toFixed(0),
-    food: (30 + Math.random() * 70).toFixed(0)
+    heartRate: 81,
+    water: 45,
+    food: 86
   });
 });
 
-// Start server
+// ✅ ADD THIS
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dashboard.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
